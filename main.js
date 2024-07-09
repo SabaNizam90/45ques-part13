2fd0b24940da6eafa03ef08fb3fbce5a1a0bf35e
Saba Nizam <160767347+SabaNizam90@users.noreply.github.com>
2024-07-10 00:02:51 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+//Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+var transportation = ["Audi", "Civic", "Honda Motorcycle", "Mehran", "Vigo", "Land cruiser"];
+transportation.map(function (items) { return console.log("I would like to own a ".concat(items)); });
